2b314f5853ededf02d4d28b1c630e42cacaede1d
叶以承 <68099595+ye-yi-cheng@users.noreply.github.com>
2021-01-25 23:49:46 +0800

    Add files via upload

---
+++ Your Ride Is Here.cpp
@@ -0,0 +1,19 @@
+/*
+ID: yeyiche1
+TASK: ride 
+LANG: C++                 
+*/
+#include<bits/stdc++.h>
+int main(){
+	freopen("ride.in","r",stdin);
+	freopen("ride.out","w",stdout);
+    long x=1,y=1;   
+	char a[7],b[7];
+    scanf("%s%s",&a,&b); 
+    int i=0;
+    for (;a[i]!=0;i++)x*=(a[i]-64);
+    for (i=0;b[i]!=0;i++)y*=(b[i]-64);
+    if(x%47==y%47)printf("GO\n");
+	else printf("STAY\n");
+	return 0;
+}
