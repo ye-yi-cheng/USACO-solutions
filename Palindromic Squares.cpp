48915b7752e5244ceec1a9b154d4c38d3ce672be
叶以承 <68099595+ye-yi-cheng@users.noreply.github.com>
2021-01-31 09:48:38 +0800

    Update Palindromic Squares.cpp

--- Palindromic Squares.cpp
+++ Palindromic Squares.cpp
@@ -1,56 +1,63 @@
 #include<bits/stdc++.h>
 using namespace std;
 int base;
+char chara[101];
 
-bool pal(int num){
-	int n;
+
+void trans(int num,int base){
 	int a[101];
-	bool flag=true;
-	for(int i=0;;i++){
-		if(num>=pow(10,i)&&num<pow(10,i+1)){
-			n=i+1;
-			break;
+	int n=0;
+	while(num>=base){
+		a[n]=num%base;
+		num/=base;
+		n++;
+	}
+	a[n]=num%base;
+	n++;
+	if(n>1){
+		for(int i=0;i<n/2;i++){
+			swap(a[i],a[n-i-1]);
 		}
 	}
 	for(int i=0;i<n;i++){
-		a[i]=(num%int(pow(10,n-i+1)))/pow(10,n-i);
+		if(a[i]<10){
+			chara[i]='0'+a[i];
+		}else{
+			chara[i]='A'+a[i]-10;
+		}
 	}
+}
+
+bool pal(){
+	int n=strlen(chara);
+	bool flag=true;
 	for(int i=0;i<n/2;i++){
-		if(a[i]!=a[n-i-1]){
+		if(chara[i]!=chara[n-i-1]){
 			flag=false;
 			break;
-		} 
+		}
 	}
 	return flag;
 }
 
-int trans(int num,int base){
-	int n;
-	int a[101];
-	for(int i=0;;i++){
-		if(num>=pow(10,i)&&num<pow(10,i+1)){
-			n=i+1;
-			break;
-		}
-	}
-	int i=0;
-	while(num>=base){
-		a[i]=num%base;
-		num%=base;
-	}
-	for(int j=0;j<i;j++){
-		num+=a[j]*pow(10,j);
-	}
-	return num;
-}
-
 int main(){
 	scanf("%d",&base);
-	printf("help");
 	for(int i=0;i<300;i++){
-		printf("%d\n",trans(pow(i,2),base));
-		if(pal(trans(pow(i,2),base))==true){
-			printf("%d %d\n",trans(i,base),trans(pow(i,2),base));
+		for(int j=0;j<100;j++){
+			chara[j]='\0';
+		}
+		trans(pow(i,2),base); 
+		if(pal()==true){
+			for(int j=0;j<100;j++){
+				chara[j]='\0';
+			}
+			trans(i,base);
+			printf("%s ",chara);
+			for(int j=0;j<100;j++){
+				chara[j]='\0';
+			}
+			trans(i*i,base);
+			printf("%s\n",chara);
 		}
 	}
 	return 0;
