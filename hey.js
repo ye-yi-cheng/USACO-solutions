b33d0a519d790d2dfbad5ddc80c6ac8f7e72ef98
叶以承 <68099595+ye-yi-cheng@users.noreply.github.com>
2021-02-01 13:40:04 +0800

    Create hey.js

---
+++ hey.js
@@ -0,0 +1,333 @@
+
+<html>
+<head>
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<title>USACO Training Gateway</title>
+<style type="text/css">
+<!--@import url('https://rsms.me/inter/inter-ui.css');-->
+body {
+    font-family: Helvetica, Sans-Serif;
+    /*font-family: 'Inter UI', sans-serif;*/
+    background-color:#e0f0e0;
+}
+</style>
+</head>
+
+<body>
+
+	<!--------- header table/box ----------->
+<table align="center">
+<tr>
+	   <!-- column 1 (cowhead logo) -->
+   <td align=center valign=middle><img src="/usaco/cowhead2.gif"></td>
+	   <!-- column 2 ('usaco training') -->
+   <td align=center valign=middle>
+		<b style='font-size:150%;'>USACO<BR>TRAINING</b></td>
+	   <!-- column 3 (solved/users_online box) -->
+   <td valign='middle' bgcolor="#007000"
+       style='border-style:solid;border-width:1px;'><table>
+<tr><td align=center><font color='white'><b>1,567,408 Tasks Solved</b></font></td></tr>
+
+<tr><td align=center><b><font color="white">
+<font face='Helvetica,Verdana,Tahoma,sans-serif,Arial,Lucida Sans,Gill Sans'>
+6 users online</font></b></td></tr>
+<tr><td align=center><font color="white">
+<font face='Helvetica,Verdana,Tahoma,sans-serif,Arial,Lucida Sans,Gill Sans'>
+ CHN/3 IND/1 USA/2</font></td></tr></table>
+</td>
+	   <!-- column 4 (spacer) -->
+   <td><div style='width:15px;'></div></td>
+	   <!-- column 5 (navigation, i) -->
+   <td>
+	<a href="/usacogate?a=DN6GmZboFWm">Refresh this page</a><br>
+
+	<form name='editinfo' style='padding:0;margin:0;' method='post'
+			action='/usacoedit'>
+	   <input type='hidden' name=a value='DN6GmZboFWm'>
+    	   <a href="javascript:document.forms['editinfo'].submit();">
+			Edit Personal Info</a>
+	</form>
+
+	<form name='chpw' style='padding:0;margin:0;' method='post'
+			action='/usaconewpw'>
+	   <input type='hidden' name=a value='DN6GmZboFWm'>
+    	   <a href="javascript:document.forms['chpw'].submit();">
+			Change Password</a>
+	</form>
+
+	
+	<a href="https://train.usaco.org/usaco/privacy.htm">Training Privacy Policy</a><br>
+   </td>
+	   <!-- column 6 (spacer) -->
+   <td><div style='width:15px;'></div></td>
+	   <!-- column 7 (navigation, ii) -->
+   <td>
+	<a href="/usacologout?a=DN6GmZboFWm">Logout</a><br>
+	<a href="/usacogate?a=DN6GmZboFWm&changelang=1">Change&nbsp;Language</a><br>
+	<a href="http://www.usaco.org">USACO HOME</a><br>
+        
+   </td>
+	   <!-- column 8 (spacer) -->
+   <td><div style='width:15px;'></div></td>
+	   <!-- column 9 (versions) -->
+   <td style='font-size:80%;'>
+<center>Compiler Versions</center>
+gcc version 9.3.0<br>
+Python 3.8.2<br>
+Python 2.7.18rc1<br>
+Java 8.0_111<br>
+Free Pascal 3.0.4+dfsg-23
+   </td>
+</table>
+
+
+
+</center>
+<div style='height:3px;'></div>
+
+<!---- Banner headline news
+<center>
+<table bgcolor="blue">
+<tr><td align=center><font size=4 color=white><b>New server. Please send issues to rob.kolstad@gmail.com .</b></font></td></tr>
+</table>
+</center>
+--->
+
+<form method='post' action='/usacogate'><input type='hidden' name=a value='DN6GmZboFWm'>
+<div style='height:3px;'></div>
+
+<!------------
+<font color=red>
+<b><center>PYTHON 2 & 3 NOW AVAILABLE [LANG: PYTHON2 & LANG: PYTHON3]</center></b>
+</font>
+<center>Please send Python problems or surprises to <a href='mailto:rob.kolstad@gmail.com'>
+rob.kolstad@gmail.com</a> along with your USACO training ID.</center>
+<center style='margin:0;padding:0;'><i>See Section 1.1 'Submitting Solutions' for sample Python files.</i></center>
+--------->
+
+<!--
+<center>
+<div style='height:0.05in;'>&nbsp;</div>
+<div style='color:purple;font-weight:bold;padding-bottom:0.05in;'>
+16 Apr 2020: Short (30 min?) outage today for fiber to be 
+pulled through conduit.<br>Problems/comments to
+<a href='mailto:rob.kolstad@gmail.com'>rob.kolstad@gmail.com</a>.
+</div>
+</center>
+-->
+
+<center>
+Personalized Curriculum for <b>Yicheng Ye</b> (yeyiche1); Last visit:  3 minutes ago
+<br>
+
+<!---
+<div style='background:black;width:65%;'>
+	<div style='height:5px;'></div>
+<table>
+<tr>
+  <td align=center width=20% valign=middle><b><font color=yellow size=2>15 July 2018</font></b></td>
+  <td><font color=yellow> <b>Huge system upgrade. Plenty of
+	software changed without notice and has caused nasty
+	bugs. Please report issues to rob.kolstad@gmail.com for repair.</b></td>
+</tr>
+</table>
+<div style='height:5px;'></div>
+</div>
+-->
+
+</center>
+</form>
+<center>
+<b>Next Problem: <a href=/usacoprob2?a=DN6GmZboFWm&S=sprime>SuperPrime Rib</a></b>
+</center>
+
+  <div style='height:7px;'></div>
+<center>
+
+<table>
+<tr>
+	<!-- news (left column) -->
+<td style='border-style:solid;border-width:1px;padding:6px;' width='220'
+           valign='top' bgcolor='#EEFFEE'><center><b><big>NEWS</big></b></center>
+<font color="#ff0000"></font>
+<table>
+<tr><td>
+<a href=http://www.usaco.org/index.php?page=contests> 2020-2021 Contest Schedule</a></td></tr>
+<tr><td><ul style='padding:0 0 0 18px;'>
+<!--<li>Jan 19-22: Second Contest </li>-->
+<!--<li>Feb 23-26: Third Contest </li>-->
+<!--<li>Mar 23-26: US Open </li>-->
+<!--<li><a href='http://www.usaco.org/index.php?page=finalists18'>May 24-Jun 2: Training Camp</a></li>-->
+<li><font color='#000'><b>Dec 18-21: Contest #1</b></font></li>
+<li><font color='#000'>Jan 22-25: Contest #2</font></li>
+<li><font color='#000'>Feb 26-Mar 1: Contest #3</font></li>
+<li><font color='#000'>Apr 2-5: US Open</font></li>
+<li><font color='#000'>Late May?: Training Camp</font></li>
+<li><font color='#000'>Jun 20-27: IOI 2021 in Singapore</font></li>
+</ul>
+</td></tr></table>
+Other open competitions:<br>
+<ul style="padding-left:18px;margin-top:0px;">
+<li><a href=http://www.codecup.nl target="_blank">The CodeCup Competition</a></li>
+<li><a href=http://hsin.hr/coci/ target="_blank">Croatian Competition (monthly)</a></li>
+<li><a href="https://www.topcoder.com" target="_blank">Topcoder</a></li>
+<li><a href=https://www.hackerrank.com/ target="_blank">HackerRank</a></li>
+<li><a href=http://codeforces.com/ target="_blank">CodeForces</a></li>
+<li><a href=https://codingcompetitions.withgoogle.com target="_blank">Google Competitions [3 contests] (new!)</a></li>
+<li><a href=https://en.wikipedia.org/wiki/Competitive_programming target="_blank">Others (@Wikipedia)</a></li>
+</ul>
+Other training sites:<br>
+<ul style="padding-left:18px;margin-top:0px;margin-bottom:7px;">
+<li><a href=https://coderbyte.com target="_blank">coderbyte</a></li>
+<li><a href=https://www.codechef.com/ target="_blank">CodeChef *</a></li>
+<li><a href=https://leetcode.com/ target="_blank">LeetCode</a></li>
+<li><a href=http://www.spoj.com/ target="_blank">Sphere online judge *</a></li>
+<li><a href=https://exercism.io/ target="_blank">exercism (new!)</a></li>
+<li><a href=https://codewars.com/ target="_blank">codewars (new!)</a></li>
+<li><a href=https://codegym.cc/ target="_blank">CodeGym [Java] (new!)</a></li>
+<li><a href=https://codepen.io/ target="_blank">Codepen (new!)</a></li>
+<li><a href=https://codingame.com/ target="_blank">CodinGame [Fun] (new!)</a></li>
+<li><a href=https://www.urionlinejudge.com.br/ target="_blank">URI Online (new!)</a></li>
+<li><a href=https://challenges.reply.com/ target="_blank">REPLY Challenges (new!) *</a></li>
+</ul>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;* Contests, too
+</font>
+</td>
+<td><div style='width:5px;'></div></td>
+
+	<!-- navigation (right column) -->
+<td valign='top' style='padding:0;'>
+<table style='border-style:solid; border-width:1px;'
+	bgcolor='#EEFFEE'  cellpadding=2 cellspacing=0><tr bgcolor='blue'>
+  <td colspan=4 style='color:yellow;font-size:80%;' align=center><b>CHAPTER 1</b></td>
+</tr>
+<tr bgcolor=#e8e8f0>
+  <td rowspan=1 style='border-style:solid; border-width:
+	1px 0 0 2px; border-left-color:blue; ' align=center>Section 1.1</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 1px;'>DONE</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 0;'>2021.01.10</td>
+  <td style='border-style:solid;border-width:
+	1px 2px 0 0; border-right-color:blue;
+	'><a href=/usacogate?a=DN6GmZboFWm&C=1.1>Introduction&nbsp;<span style='font-size:70%;font-weight:bold;position:relative; top:-2px;'>[1 Items]</span></a>
+</td>
+</tr>
+<tr bgcolor=#e8e8f0>
+  <td rowspan=1 style='border-style:solid; border-width:
+	1px 0 0 2px; border-left-color:blue; ' align=center>Section 1.2</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 1px;'>DONE</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 0;'>2021.01.14</td>
+  <td style='border-style:solid;border-width:
+	1px 2px 0 0; border-right-color:blue;
+	'><a href=/usacogate?a=DN6GmZboFWm&C=1.2>Submitting Soln, Task Types, Ad Hoc&nbsp;<span style='font-size:70%;font-weight:bold;position:relative; top:-2px;'>[7 Items]</span></a>
+</td>
+</tr>
+<tr bgcolor=#e8e8f0>
+  <td rowspan=1 style='border-style:solid; border-width:
+	1px 0 0 2px; border-left-color:blue; ' align=center>Section 1.3</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 1px;'>DONE</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 0;'>2021.01.11</td>
+  <td style='border-style:solid;border-width:
+	1px 2px 0 0; border-right-color:blue;
+	'><a href=/usacogate?a=DN6GmZboFWm&C=1.3>Complete Search&nbsp;<span style='font-size:70%;font-weight:bold;position:relative; top:-2px;'>[6 Items]</span></a>
+</td>
+</tr>
+<tr bgcolor=#e8e8f0>
+  <td rowspan=1 style='border-style:solid; border-width:
+	1px 0 0 2px; border-left-color:blue; ' align=center>Section 1.4</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 1px;'>DONE</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 0;'>2021.01.27</td>
+  <td style='border-style:solid;border-width:
+	1px 2px 0 0; border-right-color:blue;
+	'><a href=/usacogate?a=DN6GmZboFWm&C=1.4>Greedy, Crafting Solutions&nbsp;<span style='font-size:70%;font-weight:bold;position:relative; top:-2px;'>[8 Items]</span></a>
+</td>
+</tr>
+<tr bgcolor=#e8e8f0>
+  <td rowspan=1 style='border-style:solid; border-width:
+	1px 0 0 2px; border-left-color:blue; ' align=center>Section 1.5</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 1px;'>DONE</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 0;'>2021.01.30</td>
+  <td style='border-style:solid;border-width:
+	1px 2px 0 0; border-right-color:blue;
+	'><a href=/usacogate?a=DN6GmZboFWm&C=1.5>More Search Techniques&nbsp;<span style='font-size:70%;font-weight:bold;position:relative; top:-2px;'>[3 Items]</span></a>
+</td>
+</tr>
+<tr bgcolor='#f8f8ff'>
+  <td rowspan=4 style='border-style:solid; border-width:
+	1px 0 3px 2px; border-left-color:blue; border-bottom-color:blue;' align=center>Section 1.6</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 1px;'>DONE</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 0;'>2021.01.30</td>
+  <td style='border-style:solid;border-width:
+	1px 2px 0 0;border-right-color:blue;'><a href=/usacotext2?a=DN6GmZboFWm&S=binary>TEXT Introduction to Binary Numbers</a></td>
+</tr>
+<tr bgcolor='#f8f8ff'>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 1px;'>DONE</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 0;'>2021.01.30</td>
+    <td style='border-style:solid;border-width:
+	1px 2px 0 0; border-right-color:blue;'><a href=/usacoprob2?a=DN6GmZboFWm&S=numtri>PROB Number Triangles</a>&nbsp;<a href=/usacoanal2?a=DN6GmZboFWm&S=numtri>[ANALYSIS]</a></td>
+</tr>
+<tr bgcolor='#f8f8ff'>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 1px;'>DONE</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 0 0;'>2021.01.31</td>
+    <td style='border-style:solid;border-width:
+	1px 2px 0 0; border-right-color:blue;'><a href=/usacoprob2?a=DN6GmZboFWm&S=pprime>PROB Prime Palindromes</a>&nbsp;<a href=/usacoanal2?a=DN6GmZboFWm&S=pprime>[ANALYSIS]</a></td>
+</tr>
+<tr bgcolor='#f8f8ff'>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 3px 1px;border-bottom-color:blue;'>VIEWED</td>
+  <td align=center style='border-style:solid;border-width:
+	1px 0 3px 0;border-bottom-color:blue;'>2021.01.30</td>
+    <td style='border-style:solid;border-width:
+	1px 2px 3px 0; border-right-color:blue;border-bottom-color:blue;'><a href=/usacoprob2?a=DN6GmZboFWm&S=sprime>PROB SuperPrime Rib</a></td>
+</tr>
+<tr>
+    <td align=center>Chapter 2</td><td align=center>TODO</td>
+    <td align=center>&nbsp;</td><td>Bigger Challenges</td>
+</tr>
+<tr>
+    <td align=center>Chapter 3</td><td align=center>TODO</td>
+    <td align=center>&nbsp;</td><td>Techniques more subtle</td>
+</tr>
+<tr>
+    <td align=center>Chapter 4</td><td align=center>TODO</td>
+    <td align=center>&nbsp;</td><td>Advanced algorithms and difficult drills</td>
+</tr>
+<tr>
+    <td align=center>Chapter 5</td><td align=center>TODO</td>
+    <td align=center>&nbsp;</td><td>Serious challenges</td>
+</tr>
+<tr>
+    <td align=center>Chapter 6</td><td align=center>TODO</td>
+    <td align=center>&nbsp;</td><td>Contest Practice</td>
+</tr>
+</table><br>
+<form action="/upload3" enctype="multipart/form-data" method="POST">
+<center>
+<table style='background-color:white;padding:8px;border: 2px solid black;margin-top:5px;'>
+<tr><td>
+<b>Submit a solution:</b> <input type="file" NAME="filename">
+<input type="submit" VALUE="Send it in!">
+<input type="hidden" name=a value=DN6GmZboFWm>
+</table>
+</center>
+</form>
+
+</table>
+
+</body>
+</html>
