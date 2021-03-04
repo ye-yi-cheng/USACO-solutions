45f7290e60d59b5f330aaa89fb7c6072323d9c6b
YYC <68099595+2020020201@users.noreply.github.com>
2021-03-04 16:50:02 +0800

    Add files via upload

--- The Castle.cpp
+++ The Castle.cpp
@@ -1,90 +1,133 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,m,roomcnt=0,roomsize=1,maxer=-99999;
-int walls[51][51];
-int gox[5]={-1,0,1,0},goy[5]={0,-1,0,1};
-int pow2[5]={1,2,4,8};
+int n;
+int m;
+int roomcnt=0;
+int maxer=-99999;
+int pows[5]={1,2,4,8,0};
+int a[51][51];
 bool visited[51][51];
 
-struct Node{
-	int y,x;	
+struct node{
+	int y;
+	int x;
 };
 
-queue<Node>que;
+node movement[5];
+queue<node> que;
 
-void bfs(Node start){
-	roomsize=1;
-	while(!que.empty()){
-		Node seed;
+struct wall{
+	node pos;
+	int dir;
+	int roomsize;
+};
+
+wall maxerwall;
+
+void bfs(){
+	int roomsize=0;
+	if(visited[que.front().y][que.front().x]==false){
+		roomcnt++;
+	}
+	roomsize++;
+	while(que.empty()==false){
+		node seed;
 		seed=que.front();
 		que.pop();
-//		printf("seed.y:%d seed.x:%d\n",seed.y,seed.x);
+		node move;
 		for(int i=0;i<4;i++){
-			int yy=seed.y+goy[i],xx=seed.x+gox[i];
-//			printf("%d %d->%d %d:",seed.y,seed.x,yy,xx);
-//			printf("yy:%d xx:%d visited[%d][%d]:%d walls[%d][%d]&%d(pow2[%d])=%d\n",yy,xx,yy,xx,visited[yy][xx],seed.y,seed.x,pow2[i],i,walls[seed.y][seed.x]&pow2[i]);
-//			
-//			printf("%d %d %d %d %d %d\n",yy>=0,yy<n,xx>=0,xx<m,visited[yy][xx]==false,(walls[seed.y][seed.x]&pow2[i])==0);
-			if(yy>=0&&yy<n&&xx>=0&&xx<m&&visited[yy][xx]==false&&(walls[seed.y][seed.x]&pow2[i])==0){
-				visited[yy][xx]=true;
+			move.y=seed.y+movement[i].y;
+			move.x=seed.x+movement[i].x;
+			if(move.y>=0&&move.y<n&&
+			move.x>=0&&move.x<m&&
+			visited[move.y][move.x]==false&&
+			(a[seed.y][seed.x]&pows[i])==false){
 				roomsize++;
-//				printf("%d %dÄÜµ½%d %d(roomsize:%d)\n",seed.y,seed.x,yy,xx,roomsize);
-				Node tmp;
-				tmp.y=yy;
-				tmp.x=xx;
-				que.push(tmp);
+				visited[move.y][move.x]=true;
+				que.push(move);
+				visited[move.y][move.x]=false;
 			}
 		}
 	}
-	if(roomsize>maxer){
-		maxer=roomsize;
+}
+
+int query(){
+	maxer=-99999;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			node tmp={i,j};
+			que.push(tmp);
+			bfs();
+		}
 	}
-//	printf("roomsize:%d\n",roomsize);
-	roomcnt++;
-//	for(int i=0;i<n;i++){
-//		for(int j=0;j<m;j++){
-//			printf("%d",visited[i][j]);
-//		}
-//		printf("\n");
-//	}
+}
+
+void deletewall(){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			for(int k=0;k<2;k++){
+				if(a[i][j]&pows[k]){
+					if(k==0&&i!=0){
+						a[i][j]-=pows[j];
+						if(query()>maxerwall.roomsize){
+							maxerwall={query(),k,i,j};
+						}else if(query()==maxerwall.roomsize){
+							if(i>maxerwall.pos.y&&
+							j<maxerwall.pos.x){
+								maxerwall={query(),k,i,j};
+							}else if(i==maxerwall.pos.y&&
+							j==maxerwall.pos.x){
+								if(k==0){
+									maxerwall={query(),k,i,j};
+								}
+							}
+						}
+					}else if(k==1&&j!=n-1){
+						a[i][j]-=pows[j];
+						if(query()>maxerwall.roomsize){
+							maxerwall={query(),k,i,j};
+						}else if(query()==maxerwall.roomsize){
+							if(i>maxerwall.pos.y&&
+							j<maxerwall.pos.x){
+								maxerwall={query(),k,i,j};
+							}else if(i==maxerwall.pos.y&&
+							j==maxerwall.pos.x){
+								if(k==0){
+									maxerwall={query(),k,i,j};
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
 int main(){
+	movement[0].y=-1;
+	movement[0].x=0;
+	movement[1].y=0;
+	movement[1].x=1;
+	movement[2].y=1;
+	movement[2].x=0;
+	movement[3].y=0;
+	movement[3].x=-1;
+	maxerwall.roomsize=-99999;
 	scanf("%d%d",&n,&m);
-//	printf("%d %d\n",n,m);
 	for(int i=0;i<n;i++){
 		for(int j=0;j<m;j++){
-			scanf("%d",&walls[i][j]);
+			scanf("%d",&a[i][j]);
 		}
 	}
-	
-	for(int i=0;i<51;i++){
-		for(int j=0;j<51;j++){
-			visited[i][j]=false;
-		}
+	query();
+	printf("%d\n%d\n",roomcnt,maxer);
+	deletewall();
+	printf("%d\n %d %d",maxerwall.roomsize,maxerwall.pos.y+1,maxerwall.pos.x+1);
+	if(maxerwall.dir==0){
+		printf("N\n");
+	}else{
+		printf("E\n");
 	}
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			if(visited[i][j]==false){
-				Node tmp;
-				tmp.y=i;
-				tmp.x=j;
-				que.push(tmp); 
-				visited[i][j]=true;
-				bfs(tmp);
-			}
-		}
-	}
-	printf("%d\n",roomcnt);
-	printf("%d",maxer);
 	return 0;
 }
-/*
-4
-7
-11 6 11  6  3 10  6
-7  9  6 13  5 15  5
-1 10 12  7 13  7  5
-13 11 10 8 10 12 13
-*/
-
